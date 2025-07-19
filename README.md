78fc109fa1fbbc76450bcc8ededeacafc10e29e0
MarioA <maestradaz3@gmail.com>
2025-07-19 17:31:45 -0500

    feat: varios

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # francio
 Aplicacion desktop para impresión de etiquetas transporte
-
+Nombre de la impresora para windows: ZDesigner ZD230-203dpi ZPL
 AMQP_HOST=""
 AMQP_PORT=""
 AMQP_USER=""
 AMQP_PASSWORD=""
 AMQP_VHOST=""
+
+Json de ejemplo
+{
+    "operador":"energy",
+    "guia": {
+        "id":"6160664",
+        "documento_cliente":"3636273",
+        "destinatario":"FERRETERIA EL BODEGON",
+        "direccion":"CL 50 52 A 04 22 CALLE N",
+        "destino":"ANDES, ANTIOQUIA",
+        "remitente":"DYNA Y CIA SA",
+        "zona":"POBLADO",
+        "unidades": "3"
+    }
+}
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from decouple import config
 from tkinter import ttk, messagebox, filedialog
-from zpl import Label
 import tkinter as tk
 import os
 import sys
@@ -8,8 +7,7 @@ import io
 import pika
 import threading
 import queue
-import requests
-from PIL import Image, ImageTk
+import json
 
 class MiApp:
     def __init__(self, root):
@@ -33,7 +31,7 @@ class MiApp:
         main_frame.columnconfigure(0, weight=1)
         self.lbl_bienvenida = ttk.Label(
             main_frame, 
-            text="Bienvenido a Mi App", 
+            text="Bienvenido a la App para impresión de rótulos", 
             font=('Helvetica', 14, 'bold')
         )
         self.lbl_bienvenida.grid(row=0, column=0, pady=10)
@@ -71,6 +69,7 @@ class MiApp:
             def callback(ch, method, properties, body):
                 mensaje = body.decode('utf-8')
                 print(f"Mensaje recibido: {mensaje}")
+                self.imprimir_rotulo(mensaje)
                 self.message_queue.put(mensaje)
             channel.basic_consume(queue='rotulo',on_message_callback=callback,auto_ack=True)
             
@@ -89,63 +88,102 @@ class MiApp:
             pass        
         self.root.after(100, self.verificar_mensajes)
 
-    def imprimir_rotulo(self):
+    def imprimir_rotulo(self, datos):
         try:
-            zpl_code = self.generar_rotulo()              
-            printer_name = "ZDesigner ZD230-203dpi ZPL"  # Ajusta según tu impresora            
-            if sys.platform == 'win32':
-                import win32print
-                hPrinter = win32print.OpenPrinter(printer_name)
-                try:
-                    win32print.StartDocPrinter(hPrinter, 1, ("Rótulo Guía", None, "RAW"))
-                    win32print.StartPagePrinter(hPrinter)
-                    win32print.WritePrinter(hPrinter, zpl_code.encode('utf-8'))
-                    win32print.EndPagePrinter(hPrinter)
-                    win32print.EndDocPrinter(hPrinter)
-                finally:
-                    win32print.ClosePrinter(hPrinter)
-            else:
-                # Para Linux/macOS
-                import subprocess
-                lpr = subprocess.Popen(["lpr", "-P", printer_name], stdin=subprocess.PIPE)
-                lpr.stdin.write(zpl_code.encode('utf-8'))
-                lpr.stdin.close()                
-            messagebox.showinfo("Éxito", "Rótulo enviado a la impresora")            
-        except Exception as e:
-            messagebox.showerror("Error", f"No se pudo imprimir: {str(e)}")
+            datos_dict = self.validar_estructura(datos)                        
+            guia = datos_dict['guia']
+            unidades = int(guia['unidades'])
+            for unidad in range(1, unidades + 1):
+                zpl_code = self.generar_rotulo(datos_dict, str(unidad))                                                        
+                '''if sys.platform == 'win32':
+                    printer_name = "ZDesigner ZD230-203dpi ZPL"
+                    import win32print
+                    hPrinter = win32print.OpenPrinter(printer_name)
+                    try:
+                        win32print.StartDocPrinter(hPrinter, 1, ("Rótulo Guía", None, "RAW"))
+                        win32print.StartPagePrinter(hPrinter)
+                        win32print.WritePrinter(hPrinter, zpl_code.encode('utf-8'))
+                        win32print.EndPagePrinter(hPrinter)
+                        win32print.EndDocPrinter(hPrinter)
+                    finally:
+                        win32print.ClosePrinter(hPrinter)
+                else:
+                    # Para Linux/macOS 
+                    printer_name = "ZTC-ZD230-203dpi-ZPL"
+                    import subprocess
+                    lpr = subprocess.Popen(["lpr", "-P", printer_name], stdin=subprocess.PIPE)
+                    lpr.stdin.write(zpl_code.encode('utf-8'))
+                    lpr.stdin.close()'''
+            print("Se enviaron los rótulos a la impresora correctamente.")            
+        except Exception as e:            
             print(f"No se pudo imprimir: {str(e)}")
     
-    def generar_rotulo(self):        
+    def generar_rotulo(self, datos, unidad):        
         # Visualizador https://labelary.com/viewer.html
         # Documentacion https://labelary.com/zpl.html 
-        guia = '6160664'
-        documento_cliente = '3636273'
-        destinatario = 'FERRETERIA EL BODEGON'
-        direccion = 'CL 50 52 A 04 22 CALLE N'
-        destino = 'ANDES, ANTIOQUIA'
-        remitente = 'DYNA Y CIA SA'
-        zona = 'POBLADO'
-        unidad = 1
-        unidades = 1
-        x = 2
-        # dots (1mm ≈ 8 dots)
-        zpl_code = f"""            
-            ^XA
-            ^MMT
-            ^PW480
-            ^LL0240
-            ^FO380,130^BQN,2,4^FDQA,{guia}U{unidad}^FS
-            ^FO2,40^A0,30^FDGUIA No. {guia}^FS
-            ^FO2,70^A0,30^FDDOC CLIENTE: {documento_cliente}^FS
-            ^FO2,100^A0,20^FDDESTINATARIO: {destinatario}^FS
-            ^FO2,120^A0,20^FDDIRECCION: {direccion}^FS
-            ^FO2,140^A0,20^FDDESTINO: {destino}^FS
-            ^FO2,160^A0,20^FDREMITENTE: {remitente}^FS
-            ^FO2,180^A0,20^FDZONA: {zona}^FS
-            ^FO150,180^A0,20^FDPIEZA {unidad}/{unidad}^FS
-            ^XZ
-        """
+        guia = datos['guia']
+        id = guia['id']
+        documento_cliente = guia['documento_cliente']
+        destinatario = guia['destinatario']
+        direccion = guia['direccion']
+        destino = guia['destino']
+        remitente = guia['remitente']
+        zona = guia['zona']        
+        unidades = guia['unidades']               
+        zpl_code = f"""
+^XA
+^MMT
+^PW480
+^LL0240
+^FO380,130^BQN,2,4^FDQA,{id}U{unidad}^FS
+^FO2,40^A0,30^FDGUIA No. {id}^FS
+^FO2,70^A0,30^FDDOC CLIENTE: {documento_cliente}^FS
+^FO2,100^A0,20^FDDESTINATARIO: {destinatario}^FS
+^FO2,120^A0,20^FDDIRECCION: {direccion}^FS
+^FO2,140^A0,20^FDDESTINO: {destino}^FS
+^FO2,160^A0,20^FDREMITENTE: {remitente}^FS
+^FO2,180^A0,20^FDZONA: {zona}^FS
+^FO150,180^A0,20^FDPIEZA {unidad}/{unidades}^FS
+^XZ
+            """                              
         return zpl_code.strip() 
+
+    def validar_estructura(self, datos):
+        try:
+            datos_dict = json.loads(datos)
+        except json.JSONDecodeError:
+            raise ValueError("El texto proporcionado no es un JSON válido")
+        if not isinstance(datos_dict, dict):
+            raise ValueError("El JSON debe ser un objeto (diccionario)")
+        
+        required_fields = {
+            "operador": str,
+            "guia": {
+                "id": str,
+                "documento_cliente": str,
+                "destinatario": str,
+                "direccion": str,
+                "destino": str,
+                "remitente": str,
+                "zona": str,
+                "unidades": str
+            }
+        }
+        
+        def validate_structure(data, template):
+            for key, value_type in template.items():
+                if key not in data:
+                    raise ValueError(f"Falta el campo obligatorio: {key}")
+                
+                if isinstance(value_type, dict):
+                    if not isinstance(data[key], dict):
+                        raise ValueError(f"El campo {key} debe ser un objeto")
+                    validate_structure(data[key], value_type)
+                else:
+                    if not isinstance(data[key], value_type):
+                        raise ValueError(f"El campo {key} debe ser de tipo {value_type.__name__}")
+        validate_structure(datos_dict, required_fields)
+        return datos_dict
 
 if __name__ == "__main__":
     root = tk.Tk()
